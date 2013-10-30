3dca67eb0a69513bf250bf2084252f30786616dc
sksamuel <sam@sksamuel.com>
2013-10-30 01:30:13 +0000

    Added splitter

---
+++ src/main/scala/com/sksamuel/akka/patterns/Splitter.scala
@@ -0,0 +1,15 @@
+package com.sksamuel.akka.patterns
+
+import akka.actor.{ActorRef, Actor}
+
+/**
+ * Splits up collection based messages and forwards singularly.
+ *
+ * @author Stephen Samuel */
+class Splitter(target: ActorRef) extends Actor {
+
+  def receive = {
+    case iter: Iterable[_] => iter.foreach(target !)
+    case other: AnyRef => target ! other
+  }
+}
