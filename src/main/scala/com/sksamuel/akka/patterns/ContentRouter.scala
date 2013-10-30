96d5bfb07aaca71356829856816d6bd84bf4dcd8
sksamuel <sam@sksamuel.com>
2013-10-30 11:22:26 +0000

    Added content router tests

--- src/main/scala/com/sksamuel/akka/patterns/ContentRouter.scala
+++ src/main/scala/com/sksamuel/akka/patterns/ContentRouter.scala
@@ -1,14 +1,16 @@
 package com.sksamuel.akka.patterns
 
 import akka.actor.Actor
+import scala._
+import scala.Some
 
 /** @author Stephen Samuel */
-class ContentRouter(_routes: Route*) extends Actor {
+class ContentRouter(_routes: Route[_]*) extends Actor {
 
   var routes = Map.empty[Class[_], Any => Unit]
   _routes.foreach(add)
 
-  def add(route: Route): Unit = routes = routes + (route.messageType -> route.f)
+  def add(route: Route[_]): Unit = routes = routes + (route.messageType -> route.f.asInstanceOf[(Any => Unit)])
 
   def receive = {
     case any: Any => routes.get(any.getClass) match {
@@ -18,4 +20,7 @@ class ContentRouter(_routes: Route*) extends Actor {
   }
 }
 
-case class Route(messageType: Class[_], f: Any => Unit)
+case class Route[T](messageType: Class[T], f: T => Unit)
+object Route {
+  def apply[T: Manifest](f: T => Unit) = new Route(manifest.runtimeClass.asInstanceOf[Class[T]], f)
+}
---
+++ src/test/scala/com/sksamuel/akka/patterns/ContentRouterTest.scala
@@ -0,0 +1,32 @@
+package com.sksamuel.akka.patterns
+
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, OneInstancePerTest}
+import akka.testkit.{TestProbe, TestActorRef}
+import akka.actor.ActorSystem
+import scala.concurrent.duration._
+
+/** @author Stephen Samuel */
+class ContentRouterTest extends FlatSpec with MockitoSugar with OneInstancePerTest {
+
+  implicit val system = ActorSystem()
+  val probe1 = TestProbe()
+  val probe2 = TestProbe()
+  val route1 = Route((x: String) => probe1.ref ! x)
+  val route2 = Route((x: BigDecimal) => probe2.ref ! x)
+
+  val actorRef = TestActorRef(new ContentRouter(route1, route2))
+
+  "a content router" should "send messages based on class registration" in {
+    actorRef ! "foo"
+    probe1.expectMsg("foo")
+    actorRef ! BigDecimal(10)
+    probe2.expectMsg(BigDecimal(10))
+  }
+
+  "a content router" should "ignore non registered types" in {
+    actorRef ! new Object
+    probe1.expectNoMsg(1 seconds)
+    probe2.expectNoMsg(1 seconds)
+  }
+}
