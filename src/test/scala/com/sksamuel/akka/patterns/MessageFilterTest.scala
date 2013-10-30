8aac97c5b913fa1f7c7c0cdeda5620ce778997d9
sksamuel <sam@sksamuel.com>
2013-10-30 09:55:24 +0000

    Added message filter test

---
+++ src/test/scala/com/sksamuel/akka/patterns/MessageFilterTest.scala
@@ -0,0 +1,23 @@
+package com.sksamuel.akka.patterns
+
+import org.scalatest.{OneInstancePerTest, FunSuite}
+import org.scalatest.mock.MockitoSugar
+import akka.actor.ActorSystem
+import akka.testkit.{TestActorRef, TestProbe}
+import scala.concurrent.duration._
+
+/** @author Stephen Samuel */
+class MessageFilterTest extends FunSuite with MockitoSugar with OneInstancePerTest {
+
+  implicit val system = ActorSystem()
+  val probe = TestProbe()
+  val actorRef = TestActorRef(new MessageFilter((x: Any) => x.isInstanceOf[String], probe.ref))
+
+  test("message filter accepts messages that meet filter criteria") {
+    actorRef ! new Object
+    probe.expectNoMsg(1 seconds)
+    actorRef ! "foo"
+    val msg = probe.expectMsg("foo")
+    assert("foo" === msg)
+  }
+}
