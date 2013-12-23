fa4bc33073c383fb6a210d7b40c1b4c34fe8c45c
sksamuel <sam@sksamuel.com>
2013-12-23 07:41:27 +0000

    Updated ack actor to use decorating trait

--- src/main/scala/com/sksamuel/akka/patterns/AcknowledgingActor.scala
+++ src/main/scala/com/sksamuel/akka/patterns/AcknowledgingActor.scala
@@ -1,20 +1,16 @@
 package com.sksamuel.akka.patterns
 
-import akka.actor.Actor
-
 /**
  * The AcknowledgingActor will send an ack to the sender as soon as a message is received
  * before continuing with processing the message.
- * Uses the stackable trait pattern.
+ * Uses the stackable trait pattern with DecoratingActor. Override receiver in traits that wish to use this actor.
+ * See DecoratingActor.
  * This actor is most often used as the other end to the flow control actors.
  *
  * @author Stephen Samuel */
-trait AcknowledgingActor extends Actor {
-  def behavior: PartialFunction[Any, Unit]
-  def receive = {
-    case msg: AnyRef =>
-      sender ! Acknowledged
-      behavior.apply(msg)
+trait AcknowledgingActor extends DecoratingActor {
+  receiver {
+    case msg: AnyRef => sender ! Acknowledged
   }
 }
 
