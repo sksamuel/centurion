1d6bbbb7554ec91631f6a1c9c13a50107aff5d4a
sksamuel <sam@sksamuel.com>
2019-05-10 01:50:59 -0500

    Implemented more parquet to reactivehive

--- reactive-hive-schemas-parquet/src/main/kotlin/com/sksamuel/reactivehive/ParquetSchemas.kt
+++ reactive-hive-schemas-parquet/src/main/kotlin/com/sksamuel/reactivehive/ParquetSchemas.kt
@@ -1,9 +1,28 @@
 package com.sksamuel.reactivehive
 
+import org.apache.parquet.schema.GroupType
+import org.apache.parquet.schema.MessageType
 import org.apache.parquet.schema.OriginalType
 import org.apache.parquet.schema.PrimitiveType
 
 object ParquetSchemas {
+
+  fun fromParquet(type: org.apache.parquet.schema.Type): Type {
+    return when (type) {
+      is PrimitiveType -> fromPrimitiveType(type)
+      is MessageType -> fromGroupType(type)
+      is GroupType -> fromGroupType(type)
+      else -> throw UnsupportedOperationException()
+    }
+  }
+
+  fun fromGroupType(messageType: GroupType): StructType {
+    val fields = messageType.fields.map {
+      val fieldType = fromParquet(it)
+      StructField(it.name, fieldType, it.repetition == org.apache.parquet.schema.Type.Repetition.OPTIONAL)
+    }
+    return StructType(fields)
+  }
 
   fun fromPrimitiveType(type: PrimitiveType): Type {
     return when (type.primitiveTypeName) {
--- reactive-hive-schemas/src/main/kotlin/com/sksamuel/reactivehive/Struct.kt
+++ reactive-hive-schemas/src/main/kotlin/com/sksamuel/reactivehive/Struct.kt
@@ -1,8 +1,53 @@
 package com.sksamuel.reactivehive
 
+/**
+ * Types are inspired by Apache Spark.
+ * https://github.com/apache/spark/blob/630e25e35506c02a0b1e202ef82b1b0f69e50966/sql/catalyst/src/main/scala/org/apache/spark/sql/types/DataType.scala
+ */
 interface Type
 
-data class Struct(val schema: StructType, val values: List<Any>) : Type
+data class StructType(val fields: List<StructField>) : Type {
+
+  constructor(vararg fields: StructField) : this(fields.toList())
+
+  fun indexOf(name: String): Int = fields.indexOfFirst { it.name == name }
+
+  fun fieldAt(index: Int): StructField = fields[index]
+
+  /**
+   * Returns a new StructType which is the same as the existing struct, but with the
+   * given field added to the end of the existing fields.
+   */
+  fun addField(field: StructField): StructType {
+    require(!fields.any { it.name == field.name }) { "Field ${field.name} already exists" }
+    return copy(fields = this.fields + field)
+  }
+
+  /**
+   * Returns a new [StructType] which is the same as the existing struct, but with
+   * the matching field removed, if it exists.
+   *
+   * If the field does not exist then it is a no-op.
+   */
+  fun removeField(name: String): StructType {
+    return StructType(fields.filterNot { it.name == name })
+  }
+}
+
+data class StructField(val name: String,
+                       val type: Type,
+                       val nullable: Boolean = true) {
+
+  /**
+   * Returns a new StructField with the same name as this field, but lowercased.
+   * All other properties remain the same.
+   */
+  fun toLowerCase(): StructField = copy(name = name.toLowerCase())
+
+  fun withNullable(nullable: Boolean): StructField = copy(nullable = nullable)
+
+  fun nullable(): StructField = copy(nullable = true)
+}
 
 object BooleanType : Type
 object StringType : Type
@@ -18,6 +63,15 @@ object TimestampMicrosType : Type
 object TimeMicrosType : Type
 object TimeMillisType : Type
 
+object DateType : Type
+
+data class MapDataType(val keyType: Type, val valueType: Type) : Type
+
+data class DecimalType(val precision: Precision, val scale: Scale) : Type
+
 data class EnumType(val values: List<String>) : Type
 
 data class ArrayType(val elementType: Type) : Type
+
+data class Precision(val value: Int)
+data class Scale(val value: Int)
