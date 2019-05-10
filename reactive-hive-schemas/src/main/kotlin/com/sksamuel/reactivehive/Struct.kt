b8fa7291916007e7759d34a78411becb1b874634
sksamuel <sam@sksamuel.com>
2019-05-10 01:30:14 -0500

    Added some basic types

--- reactive-hive-schemas/src/main/kotlin/com/sksamuel/reactivehive/Struct.kt
+++ reactive-hive-schemas/src/main/kotlin/com/sksamuel/reactivehive/Struct.kt
@@ -1,3 +1,17 @@
 package com.sksamuel.reactivehive
 
-data class Struct(val schema: StructType, val values: List<Any>)
+interface Type
+
+data class Struct(val schema: StructType, val values: List<Any>) : Type
+
+object BooleanType : Type
+object StringType : Type
+object BinaryType : Type
+object DoubleType : Type
+object FloatType : Type
+object ByteType : Type
+object IntType : Type
+object LongType : Type
+object ShortType : Type
+
+data class ArrayType(val elementType: Type) : Type
