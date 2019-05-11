368e8139f4d55948f1a00b41ca3bc8bc4f2fd702
sksamuel <sam@sksamuel.com>
2019-05-10 21:46:22 -0500

    Added akka streams parquet source

--- reactivehive-components/reactivehive-components-akkastream/src/main/kotlin/com/sksamuel/reactivehive/akka/streams/ParquetSource.kt
+++ reactivehive-components/reactivehive-components-akkastream/src/main/kotlin/com/sksamuel/reactivehive/akka/streams/ParquetSource.kt
@@ -1,15 +1,14 @@
 package com.sksamuel.reactivehive.akka.streams
 
 import akka.stream.Attributes
-import akka.stream.Inlet
-import akka.stream.SinkShape
+import akka.stream.Outlet
+import akka.stream.SourceShape
 import akka.stream.stage.GraphStageLogic
 import akka.stream.stage.GraphStageWithMaterializedValue
-import akka.stream.stage.InHandler
+import akka.stream.stage.OutHandler
 import com.sksamuel.reactivehive.Struct
 import com.sksamuel.reactivehive.StructType
-import com.sksamuel.reactivehive.parquet.ToParquetSchema
-import com.sksamuel.reactivehive.parquet.parquetWriter
+import com.sksamuel.reactivehive.parquet.parquetReader
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import scala.Tuple2
@@ -17,51 +16,36 @@ import java.util.concurrent.CompletableFuture
 
 class ParquetSource(val path: Path,
                     val conf: Configuration,
-                    val schema: StructType) : GraphStageWithMaterializedValue<SinkShape<Struct>, CompletableFuture<Int>>() {
+                    val schema: StructType) : GraphStageWithMaterializedValue<SourceShape<Struct>, CompletableFuture<Int>>() {
 
-  private val inlet: Inlet<Struct> = Inlet.create("ParquetSink.in")
-  override fun shape(): SinkShape<Struct> = SinkShape.of(inlet)
+  private val outlet: Outlet<Struct> = Outlet.create("ParquetSource.out")
+  override fun shape(): SourceShape<Struct> = SourceShape.of(outlet)
 
   override fun createLogicAndMaterializedValue(inheritedAttributes: Attributes?): Tuple2<GraphStageLogic, CompletableFuture<Int>> {
 
     val future = CompletableFuture<Int>()
 
-    val graph: GraphStageLogic = object : GraphStageLogic(shape()), InHandler {
+    val graph: GraphStageLogic = object : GraphStageLogic(shape()), OutHandler {
 
-      private val writer = parquetWriter(path, conf, ToParquetSchema.toMessageType(schema))
+      private val reader = parquetReader(path, conf)
       private var count = 0
 
       init {
-        setHandler(inlet, this)
+        setHandler(outlet, this)
       }
 
-      override fun beforePreStart() {
-        pull(inlet)
-      }
-
-      override fun onPush() {
-        val struct = grab(inlet)
+      override fun onPull() {
+        val struct = reader.read()
         try {
-          writer.write(struct)
-          count++
-          pull(inlet)
+          if (struct == null) {
+            completeStage()
+          } else {
+            push(outlet, struct)
+            count++
+          }
         } catch (t: Throwable) {
           failStage(t)
-          future.completeExceptionally(t)
-          writer.close()
         }
-      }
-
-      override fun onUpstreamFinish() {
-        future.complete(count)
-        writer.close()
-        completeStage()
-      }
-
-      override fun onUpstreamFailure(t: Throwable?) {
-        future.completeExceptionally(t)
-        writer.close()
-        failStage(t)
       }
     }
 
