7019ef9393a3c5358981743b80bf951436ed02bb
sksamuel <sam@sksamuel.com>
2019-05-10 21:43:42 -0500

    Addd docker

---
+++ reactivehive-components/reactivehive-components-akkastream/src/main/kotlin/com/sksamuel/reactivehive/akka/streams/ParquetSource.kt
@@ -0,0 +1,70 @@
+package com.sksamuel.reactivehive.akka.streams
+
+import akka.stream.Attributes
+import akka.stream.Inlet
+import akka.stream.SinkShape
+import akka.stream.stage.GraphStageLogic
+import akka.stream.stage.GraphStageWithMaterializedValue
+import akka.stream.stage.InHandler
+import com.sksamuel.reactivehive.Struct
+import com.sksamuel.reactivehive.StructType
+import com.sksamuel.reactivehive.parquet.ToParquetSchema
+import com.sksamuel.reactivehive.parquet.parquetWriter
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import scala.Tuple2
+import java.util.concurrent.CompletableFuture
+
+class ParquetSource(val path: Path,
+                    val conf: Configuration,
+                    val schema: StructType) : GraphStageWithMaterializedValue<SinkShape<Struct>, CompletableFuture<Int>>() {
+
+  private val inlet: Inlet<Struct> = Inlet.create("ParquetSink.in")
+  override fun shape(): SinkShape<Struct> = SinkShape.of(inlet)
+
+  override fun createLogicAndMaterializedValue(inheritedAttributes: Attributes?): Tuple2<GraphStageLogic, CompletableFuture<Int>> {
+
+    val future = CompletableFuture<Int>()
+
+    val graph: GraphStageLogic = object : GraphStageLogic(shape()), InHandler {
+
+      private val writer = parquetWriter(path, conf, ToParquetSchema.toMessageType(schema))
+      private var count = 0
+
+      init {
+        setHandler(inlet, this)
+      }
+
+      override fun beforePreStart() {
+        pull(inlet)
+      }
+
+      override fun onPush() {
+        val struct = grab(inlet)
+        try {
+          writer.write(struct)
+          count++
+          pull(inlet)
+        } catch (t: Throwable) {
+          failStage(t)
+          future.completeExceptionally(t)
+          writer.close()
+        }
+      }
+
+      override fun onUpstreamFinish() {
+        future.complete(count)
+        writer.close()
+        completeStage()
+      }
+
+      override fun onUpstreamFailure(t: Throwable?) {
+        future.completeExceptionally(t)
+        writer.close()
+        failStage(t)
+      }
+    }
+
+    return Tuple2(graph, future)
+  }
+}
