172bf128e4f6ce23894b8559e02ce5d06341500a
sksamuel <sam@sksamuel.com>
2024-04-13 17:21:20 -0500

    Added extractors

--- centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/Records.kt
+++ centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/Records.kt
@@ -7,24 +7,24 @@ import org.apache.avro.generic.IndexedRecord
 
 object Records {
 
-  fun toRecord(struct: Struct): GenericData.Record {
-    val schema = Schemas.toAvro(struct.schema)
-    val record = GenericData.Record(schema)
-    struct.iterator().forEach { (field, value) ->
-      val encoded = if (value == null) null else Encoders.encoderFor(field.schema).encode(value)
-      record.put(field.name, encoded)
-    }
-    return record
-  }
+   fun toRecord(struct: Struct): GenericData.Record {
+      val schema = Schemas.toAvro(struct.schema)
+      val record = GenericData.Record(schema)
+      struct.iterator().forEach { (field, value) ->
+         val encoded = if (value == null) null else Encoders.encoderFor(field.schema).encode(value)
+         record.put(field.name, encoded)
+      }
+      return record
+   }
 
-  fun fromRecord(record: IndexedRecord): Struct {
-    val schema = Schemas.fromAvro(record.schema) as Schema.Struct
-    val values = schema.fields.withIndex().map { (index, field) ->
-      val value = record.get(index)
-      val decoder = Decoders.decoderFor(field.schema)
-      decoder.decode(value)
-    }
-    return Struct(schema, values)
-  }
+   fun fromRecord(record: IndexedRecord): Struct {
+      val schema = Schemas.fromAvro(record.schema) as Schema.Struct
+      val values = schema.fields.withIndex().map { (index, field) ->
+         val value = record.get(index)
+         val decoder = Decoders.decoderFor(field.schema)
+         decoder.decode(value)
+      }
+      return Struct(schema, values)
+   }
 }
 
---
+++ centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/extractors.kt
@@ -0,0 +1,86 @@
+package com.sksamuel.centurion.avro
+
+import org.apache.avro.generic.GenericRecord
+import org.apache.avro.util.Utf8
+
+interface Extractor<T> {
+   fun extract(record: GenericRecord, key: String): T
+}
+
+object RequiredStringExtractor : Extractor<String> {
+   override fun extract(record: GenericRecord, key: String): String {
+      return when (val value = record.get(key)) {
+         is String -> value
+         is Utf8 -> value.toString()
+         else -> error("Unknown type $value for key $key")
+      }
+   }
+}
+
+object OptionalStringExtractor : Extractor<String?> {
+   override fun extract(record: GenericRecord, key: String): String? {
+      return when (val value = record.get(key)) {
+         null -> null
+         is String -> value
+         is Utf8 -> value.toString()
+         else -> error("Unknown type $value for key $key")
+      }
+   }
+}
+
+object RequiredDoubleExtractor : Extractor<Double> {
+   override fun extract(record: GenericRecord, key: String): Double {
+      return when (val value = record.get(key)) {
+         is Double -> value
+         else -> error("Unknown type $value for key $key")
+      }
+   }
+}
+
+object OptionalDoubleExtractor : Extractor<Double?> {
+   override fun extract(record: GenericRecord, key: String): Double? {
+      return when (val value = record.get(key)) {
+         null -> null
+         is Double -> value
+         else -> error("Unknown type $value for key $key")
+      }
+   }
+}
+
+object RequiredLongExtractor : Extractor<Long> {
+   override fun extract(record: GenericRecord, key: String): Long {
+      return when (val value = record.get(key)) {
+         is Long -> value
+         else -> error("Unknown type $value for key $key")
+      }
+   }
+}
+
+object OptionalLongExtractor : Extractor<Long?> {
+   override fun extract(record: GenericRecord, key: String): Long? {
+      return when (val value = record.get(key)) {
+         null -> null
+         is Long -> value
+         else -> error("Unknown type $value for key $key")
+      }
+   }
+}
+
+object RequiredIntExtractor : Extractor<Int> {
+   override fun extract(record: GenericRecord, key: String): Int {
+      return when (val value = record.get(key)) {
+         is Int -> value
+         else -> error("Unknown type $value for key $key")
+      }
+   }
+}
+
+object OptionalIntExtractor : Extractor<Int?> {
+   override fun extract(record: GenericRecord, key: String): Int? {
+      return when (val value = record.get(key)) {
+         null -> null
+         is Int -> value
+         else -> error("Unknown type $value for key $key")
+      }
+   }
+}
