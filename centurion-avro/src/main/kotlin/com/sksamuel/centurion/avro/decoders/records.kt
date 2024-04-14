62465932a027242fe9e6accb0ea97350883a1e9c
sksamuel <sam@sksamuel.com>
2024-04-14 15:56:44 -0500

    Added RecordEncoder / RecordDecoder wrappers

--- centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/decoders/records.kt
+++ centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/decoders/records.kt
@@ -5,24 +5,6 @@ import org.apache.avro.generic.GenericRecord
 import kotlin.reflect.KClass
 import kotlin.reflect.full.primaryConstructor
 
-class ReflectionRecordDecoder<T : Any> : Decoder<T> {
-
-   override fun decode(schema: Schema, value: Any?): T {
-      require(schema.type == Schema.Type.RECORD)
-      require(value is GenericRecord) { "ReflectionRecordDecoder only supports GenericRecords: was $value" }
-      val kclass = value::class
-      require(kclass.isData) { "Decoders only support data class: was $kclass" }
-      val args = kclass.primaryConstructor!!.parameters.map { param ->
-         val field = schema.getField(param.name)
-         val arg = value.get(param.name)
-         val decoder = Decoder.decoderFor(param.type)
-         decoder.decode(field.schema(), arg)
-      }
-      require(args.size == kclass.primaryConstructor!!.parameters.size)
-      return kclass.primaryConstructor!!.call(*args.toTypedArray()) as T
-   }
-}
-
 class SpecificRecordDecoder<T : Any>(
    private val kclass: KClass<T>,
    private val schema: Schema,
--- centurion-avro/src/test/kotlin/com/sksamuel/centurion/avro/decoders/SpecificRecordDecoderTest.kt
+++ centurion-avro/src/test/kotlin/com/sksamuel/centurion/avro/decoders/SpecificRecordDecoderTest.kt
@@ -8,7 +8,7 @@ import org.apache.avro.SchemaBuilder
 import org.apache.avro.generic.GenericData
 import org.apache.avro.util.Utf8
 
-class ReflectionRecordDecoderTest : FunSpec({
+class SpecificRecordDecoderTest : FunSpec({
 
    test("basic record decoder") {
       data class Foo(val a: String, val b: Boolean)
@@ -19,7 +19,7 @@ class ReflectionRecordDecoderTest : FunSpec({
       record.put("a", Utf8("hello"))
       record.put("b", true)
 
-      ReflectionRecordDecoder<Foo>().decode(schema, record) shouldBe Foo("hello", true)
+      SpecificRecordDecoder(Foo::class, schema).decode(schema, record) shouldBe Foo("hello", true)
    }
 
    test("enums") {
@@ -33,7 +33,7 @@ class ReflectionRecordDecoderTest : FunSpec({
       val record = GenericData.Record(schema)
       record.put("wine", GenericData.get().createEnum("Shiraz", wineSchema))
 
-      ReflectionRecordDecoder<Foo>().decode(schema, record) shouldBe Foo(Wine.Shiraz)
+      SpecificRecordDecoder<Foo>(Foo::class, schema).decode(schema, record) shouldBe Foo(Wine.Shiraz)
    }
 
    test("nulls") {
@@ -48,7 +48,7 @@ class ReflectionRecordDecoderTest : FunSpec({
       record.put("a", null)
       record.put("b", Utf8("hello"))
 
-      ReflectionRecordDecoder<Foo>().decode(schema, record) shouldBe Foo(null, "hello")
+      SpecificRecordDecoder<Foo>(Foo::class, schema).decode(schema, record) shouldBe Foo(null, "hello")
    }
 
    test("sets") {
@@ -72,7 +72,7 @@ class ReflectionRecordDecoderTest : FunSpec({
          )
       )
 
-      ReflectionRecordDecoder<Foo>().decode(schema, record) shouldBe Foo(
+      SpecificRecordDecoder<Foo>(Foo::class, schema).decode(schema, record) shouldBe Foo(
          setOf(1, 2),
          setOf(1L, null, 2L),
          setOf(Wine.Shiraz, Wine.Malbec),
@@ -100,7 +100,7 @@ class ReflectionRecordDecoderTest : FunSpec({
          )
       )
 
-      ReflectionRecordDecoder<Foo>().decode(schema, record) shouldBe Foo(
+      SpecificRecordDecoder<Foo>(Foo::class, schema).decode(schema, record) shouldBe Foo(
          listOf(1, 2),
          listOf(1L, null, 2L),
          listOf(Wine.Shiraz, Wine.Malbec),
