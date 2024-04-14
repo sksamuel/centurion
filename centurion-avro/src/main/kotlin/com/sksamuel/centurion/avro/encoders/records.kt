9f617601a2f2867a465088966950f2c9fc8d8ea1
sksamuel <sam@sksamuel.com>
2024-04-14 15:11:08 -0500

    Added SpecificRecordEncoder and benchmarks

--- centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/encoders/records.kt
+++ centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/encoders/records.kt
@@ -1,31 +1,54 @@
 package com.sksamuel.centurion.avro.encoders
 
+import io.kotest.core.Tuple4
 import org.apache.avro.Schema
 import org.apache.avro.generic.GenericData
 import org.apache.avro.generic.GenericRecord
+import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
 
 /**
- * An [Encoder] that returns a [GenericRecord] for a given data class instance, using
+ * An [Encoder] that returns a [GenericRecord] for any data class instance at runtime, using
  * reflection to access the fields of the data class.
  */
 class ReflectionRecordEncoder : Encoder<Any> {
 
    override fun encode(schema: Schema, value: Any): Any {
-      require(schema.type == Schema.Type.RECORD)
+      return SpecificRecordEncoder(value::class as KClass<Any>, schema).encode(schema, value)
+   }
+}
 
-      val kclass = value::class
+/**
+ * An [Encoder] that returns a [GenericRecord] for a given data class instance, using
+ * reflection to access the fields of the data class. In contrast to [ReflectionRecordEncoder],
+ * this encoder requires the class and schema in advance, which it uses to pre-generate some
+ * of the reflective calls needed.
+ */
+class SpecificRecordEncoder<T : Any>(
+   private val kclass: KClass<T>,
+   private val schema: Schema,
+) : Encoder<T> {
+
+   init {
       require(kclass.isData) { "Can only encode data classes: $kclass" }
+   }
+
+   private val encoders = kclass.declaredMemberProperties.map { member: KProperty1<out Any, *> ->
+      val field = schema.getField(member.name) ?: error("Could not find field ${member.name} in schema")
+      val encoder = Encoder.encoderFor(member.returnType) as Encoder<Any?>
+      Tuple4(field.schema(), encoder, member.name, member.getter)
+   }
+
+   override fun encode(schema: Schema, value: T): Any {
+      require(schema.type == Schema.Type.RECORD)
 
       val record = GenericData.Record(schema)
 
-      value::class.declaredMemberProperties.map { member: KProperty1<out Any, *> ->
-         val field = schema.getField(member.name) ?: error("Could not find field ${member.name} in schema")
-         val encoder = Encoder.encoderFor(member.returnType) as Encoder<Any?>
-         val v = member.getter.call(value)
-         val encoded = encoder.encode(field.schema(), v)
-         record.put(member.name, encoded)
+      encoders.map { (schema, encoder, name, getter) ->
+         val v = getter.call(value)
+         val encoded = encoder.encode(schema, v)
+         record.put(name, encoded)
       }
 
       return record
---
+++ centurion-avro/src/test/kotlin/com/sksamuel/centurion/avro/encoders/Benchmarks.kt
@@ -0,0 +1,65 @@
+@file:OptIn(ExperimentalTime::class)
+
+package com.sksamuel.centurion.avro.encoders
+
+import com.sksamuel.centurion.avro.generation.ReflectionSchemaBuilder
+import org.apache.avro.generic.GenericData
+import org.apache.avro.util.Utf8
+import kotlin.random.Random
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
+data class User(
+   val userId: Long,
+   val name: String,
+   val email: String,
+   val lastActiveTimestamp: Long,
+   val type: UserType
+)
+
+enum class UserType { User, Admin }
+
+fun main() {
+
+   val user = User(Random.nextLong(), "sammy mcsamface", "sammy@mcsamface.com", Random.nextLong(), UserType.Admin)
+   val schema = ReflectionSchemaBuilder().schema(User::class)
+
+   repeat(5) {
+      val time = measureTime {
+         repeat(5_000_000) {
+            ReflectionRecordEncoder().encode(schema, user)
+         }
+      }
+      println("ReflectionRecordEncoder: $time")
+   }
+
+   repeat(5) {
+      val encoder = SpecificRecordEncoder(User::class, schema)
+      val time = measureTime {
+         repeat(5_000_000) {
+            encoder.encode(schema, user)
+         }
+      }
+      println("SpecificRecordEncoder: $time")
+   }
+
+   repeat(5) {
+
+      val encoder = Encoder<User> { schema, value ->
+         val record = GenericData.Record(schema)
+         record.put("userId", value.userId)
+         record.put("name", Utf8(value.name))
+         record.put("email", Utf8(value.email))
+         record.put("lastActiveTimestamp", value.lastActiveTimestamp)
+         record.put("type", GenericData.get().createEnum("Admin", schema.getField("type").schema()))
+         record
+      }
+
+      val time = measureTime {
+         repeat(5_000_000) {
+            encoder.encode(schema, user)
+         }
+      }
+      println("SpecificEncoder: $time")
+   }
+}
