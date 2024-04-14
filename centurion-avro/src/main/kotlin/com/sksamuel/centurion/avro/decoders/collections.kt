68591b6124c90fe8117383e03f0123e7706e6db7
sksamuel <sam@sksamuel.com>
2024-04-14 10:35:55 -0500

    Added list decoder

---
+++ centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/decoders/collections.kt
@@ -0,0 +1,16 @@
+package com.sksamuel.centurion.avro.decoders
+
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericData
+
+class ListDecoder<T>(private val decoder: Decoder<T>) : Decoder<List<T>> {
+   override fun decode(schema: Schema, value: Any?): List<T> {
+      require(schema.type == Schema.Type.ARRAY)
+      return when (value) {
+         is GenericData.Array<*> -> value.map { decoder.decode(schema.elementType, it) }
+         is List<*> -> value.map { decoder.decode(schema.elementType, it) }
+         is Array<*> -> value.map { decoder.decode(schema.elementType, it) }
+         else -> error("Unsupported list type $value")
+      }
+   }
+}
