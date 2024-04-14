df6dfe71db3e34718225514f0bf2a5ced4167d46
sksamuel <sam@sksamuel.com>
2024-04-14 10:27:39 -0500

    Added string decoders

--- centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/Decoder.kt
+++ centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/Decoder.kt
@@ -1,93 +1,47 @@
 package com.sksamuel.centurion.avro
 
-import com.sksamuel.centurion.Schema
-import org.apache.avro.util.Utf8
-import java.sql.Timestamp
+import org.apache.avro.Schema
 
-interface Decoder<T> {
-  fun decode(any: Any?): T
+/**
+ * A [Decoder] typeclass is used to convert an Avro value, such as a [GenericRecord],
+ * [SpecificRecord], [GenericFixed], [EnumSymbol], or a basic type, into a specified JVM type.
+ *
+ * For example, a [Decoder<String>] would convert an input such as a GenericFixed, byte array, or Utf8
+ * into a plain JVM [String].
+ *
+ * Another example, a decoder for nullable types would handle null-based Unions.
+ */
+fun interface Decoder<T> {
+
+   fun decode(schema: Schema, value: Any?): T
+
+   fun <U> map(fn: (T) -> U): Decoder<U> {
+      val self = this
+      return Decoder { schema, value -> fn(self.decode(schema, value)) }
+   }
 }
 
-object Decoders {
-  fun decoderFor(schema: Schema): Decoder<*> = when (schema) {
-    is Schema.Array -> ListDecoder(schema.elements)
-    is Schema.Enum -> TODO()
-    is Schema.Map -> TODO()
-    is Schema.Nullable -> TODO()
-    Schema.Booleans -> BooleanDecoder
-    Schema.Bytes -> TODO()
-    is Schema.Decimal -> TODO()
-    Schema.Float32 -> TODO()
-    Schema.Float64 -> TODO()
-    Schema.Int8 -> TODO()
-    Schema.Int16 -> TODO()
-    Schema.Int32 -> Int32Decoder
-    Schema.Int64 -> Int64Decoder
-    Schema.Nulls -> TODO()
-    Schema.Strings -> StringDecoder
-    Schema.TimestampMicros -> TODO()
-    Schema.TimestampMillis -> TODO()
-    Schema.UUID -> TODO()
-    is Schema.Varchar -> TODO()
-    is Schema.Struct -> TODO()
-  }
-}
-
-class ListDecoder(schema: Schema) : Decoder<List<*>> {
-  private val decoder = Decoders.decoderFor(schema)
-  override fun decode(any: Any?): List<*> {
-    return when (any) {
-      is List<*> -> any.map { decoder.decode(it) }
-      else -> error("Cannot coerce type ${any!!::class.simpleName} to List<*>")
-    }
-  }
-
-}
-
-object StringDecoder : Decoder<String> {
-  override fun decode(any: Any?): String {
-    return when (any) {
-      is Utf8 -> any.toString()
-      is String -> any
-      else -> any.toString()
-    }
-  }
-}
-
-object BooleanDecoder : Decoder<Boolean> {
-  override fun decode(any: Any?): Boolean {
-    return when (any) {
-      is Boolean -> any
-      else -> error("Cannot coerce type ${any!!::class.simpleName} to Boolean")
-    }
-  }
-}
-
-object Int32Decoder : Decoder<Int> {
-  override fun decode(any: Any?): Int {
-    return when (any) {
-      is Int -> any
-      else -> error("Cannot coerce type ${any!!::class.simpleName} to Int")
-    }
-  }
-}
-
-object Int64Decoder : Decoder<Long> {
-  override fun decode(any: Any?): Long {
-    return when (any) {
-      is Long -> any
-      is Int -> any.toLong()
-      else -> error("Cannot coerce type ${any!!::class.simpleName} to Long")
-    }
-  }
-}
-
-object TimestampDecoder : Decoder<Timestamp> {
-  override fun decode(any: Any?): Timestamp {
-    return when (any) {
-      is Long -> Timestamp(any)
-      is Timestamp -> any
-      else -> error("Cannot coerce type ${any!!::class.simpleName} to Long")
-    }
-  }
-}
+//object Decoders {
+//   fun decoderFor(schema: Schema): Decoder<*> = when (schema) {
+//      is Schema.Array -> ListDecoder(schema.elements)
+//      is Schema.Enum -> TODO()
+//      is Schema.Map -> TODO()
+//      is Schema.Nullable -> TODO()
+//      Schema.Booleans -> BooleanDecoder
+//      Schema.Bytes -> TODO()
+//      is Schema.Decimal -> TODO()
+//      Schema.Float32 -> TODO()
+//      Schema.Float64 -> TODO()
+//      Schema.Int8 -> TODO()
+//      Schema.Int16 -> TODO()
+//      Schema.Int32 -> Int32Decoder
+//      Schema.Int64 -> Int64Decoder
+//      Schema.Nulls -> TODO()
+//      Schema.Strings -> StringDecoder
+//      Schema.TimestampMicros -> TODO()
+//      Schema.TimestampMillis -> TODO()
+//      Schema.UUID -> TODO()
+//      is Schema.Varchar -> TODO()
+//      is Schema.Struct -> TODO()
+//   }
+//}
---
+++ centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/decoders/strings.kt
@@ -0,0 +1,102 @@
+package com.sksamuel.centurion.avro.decoders
+
+import com.sksamuel.centurion.avro.Decoder
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericFixed
+import org.apache.avro.util.Utf8
+import java.nio.ByteBuffer
+
+/**
+ * A [Decoder] for [String]s that pattern match on the incoming type to decode.
+ *
+ * The schema is not used, meaning this decoder is forgiving of types that do not conform to
+ * the schema, but are nevertheless usable.
+ */
+object StringDecoder : Decoder<String> {
+   override fun decode(schema: Schema, value: Any?): String {
+      return when (value) {
+         is Utf8 -> value.toString()
+         is CharSequence -> value.toString()
+         is ByteArray -> Utf8(value).toString()
+         is ByteBuffer -> Utf8(value.array()).toString()
+         is GenericFixed -> Utf8(value.bytes()).toString()
+         else -> error("Unsupported type $value")
+      }
+   }
+}
+
+/**
+ * A [Decoder] for [CharSequence] that pattern match on the incoming type to decode.
+ *
+ * The schema is not used, meaning this decoder is forgiving of types that do not conform to
+ * the schema, but are nevertheless usable.
+ */
+val CharSequenceDecoder: Decoder<CharSequence> = StringDecoder.map { it }
+
+/**
+ * A [Decoder] for [Utf8] that pattern matches on the incoming type to decode.
+ *
+ * The schema is not used, meaning this decoder is forgiving of types that do not conform to
+ * the schema, but are nevertheless usable.
+ */
+object UTF8Decoder : Decoder<Utf8> {
+   override fun decode(schema: Schema, value: Any?): Utf8 {
+      return when (value) {
+         is Utf8 -> value
+         is CharSequence -> Utf8(value.toString())
+         is ByteArray -> Utf8(value)
+         is ByteBuffer -> Utf8(value.array())
+         is GenericFixed -> Utf8(value.bytes())
+         else -> error("Unsupported type $value")
+      }
+   }
+}
+
+/**
+ * A [Decoder] for [String]s that uses the schema to determine the expected type.
+ *
+ * Unlike [StringDecoder] this decoder is not tolerant of data that does not match the expected
+ * encoding from the schema.
+ */
+object StrictStringDecoder : Decoder<String> {
+   override fun decode(schema: Schema, value: Any?): String {
+      return when (schema.type) {
+         Schema.Type.STRING -> when (value) {
+            is String -> value
+            is Utf8 -> value.toString()
+            else -> error("Unsupported type for string schema: $schema")
+         }
+
+         Schema.Type.BYTES -> ByteStringDecoder.decode(schema, value)
+         Schema.Type.FIXED -> GenericFixedStringDecoder.decode(schema, value)
+         else -> error("Unsupported type for string schema: $schema")
+      }
+   }
+}
+
+/**
+ * A [Decoder] for Strings that decodes from [ByteBuffer]s and [ByteArray]s.
+ */
+object ByteStringDecoder : Decoder<String> {
+   override fun decode(schema: Schema, value: Any?): String {
+      require(schema.type == Schema.Type.BYTES)
+      return when (value) {
+         is ByteArray -> Utf8(value).toString()
+         is ByteBuffer -> Utf8(value.array()).toString()
+         else -> error("This decoder expects bytes but was $value")
+      }
+   }
+}
+
+/**
+ * A [Decoder] for Strings that decodes from [GenericFixed]s.
+ */
+object GenericFixedStringDecoder : Decoder<String> {
+   override fun decode(schema: Schema, value: Any?): String {
+      require(schema.type == Schema.Type.FIXED)
+      return when (value) {
+         is GenericFixed -> Utf8(value.bytes()).toString()
+         else -> error("This decoder expects GenericFixed but was $value")
+      }
+   }
+}
