580386d2e5e69a43684e8202695e095e826969b5
sksamuel <sam@sksamuel.com>
2024-04-29 07:37:46 -0500

    Added test for string decoder

---
+++ centurion-avro/src/test/kotlin/com/sksamuel/centurion/avro/decoders/StringDecoderTest.kt
@@ -0,0 +1,38 @@
+package com.sksamuel.centurion.avro.decoders
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import org.apache.avro.SchemaBuilder
+import org.apache.avro.generic.GenericData
+import org.apache.avro.util.Utf8
+import java.nio.ByteBuffer
+
+class StringDecoderTest : FunSpec({
+
+   test("decode java strings") {
+      val schema = SchemaBuilder.builder().stringType()
+      StringDecoder.decode(schema).invoke("foo") shouldBe "foo"
+   }
+
+   test("decode UTF8") {
+      val schema = SchemaBuilder.builder().stringType()
+      StringDecoder.decode(schema).invoke(Utf8("foo")) shouldBe "foo"
+   }
+
+   test("decode byte buffer") {
+      val schema = SchemaBuilder.builder().stringType()
+      StringDecoder.decode(schema).invoke(ByteBuffer.wrap("foo".encodeToByteArray())) shouldBe "foo"
+   }
+
+   test("decode byte array") {
+      val schema = SchemaBuilder.builder().bytesType()
+      StringDecoder.decode(schema).invoke("foo".encodeToByteArray()) shouldBe "foo"
+   }
+
+   test("decode fixed") {
+      val schema = SchemaBuilder.fixed("name").size(3)
+      StringDecoder.decode(schema)
+         .invoke(GenericData.get().createFixed(null, "foo".encodeToByteArray(), schema)) shouldBe "foo"
+   }
+
+})
