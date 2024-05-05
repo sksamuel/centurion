82031320fd526fa6739880a7c5dd1585dc5f0911
sksamuel <sam@sksamuel.com>
2024-05-04 20:14:00 -0500

    Updated javadocs

--- centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/io/Serde.kt
+++ centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/io/Serde.kt
@@ -4,13 +4,20 @@ import org.apache.avro.file.Codec
 import java.nio.ByteBuffer
 
 /**
- * A [Serde] provides an easy way to convert between a data class [T] and avro encoded bytes.
+ * A [Serde] provides an easy way to convert between a single data class instance [T]
+ * and avro encoded byte arrays.
  */
 interface Serde<T : Any> {
    fun serialize(obj: T): ByteArray
    fun deserialize(bytes: ByteArray): T
 }
 
+/**
+ * A [CompressingSerde] wraps another [Serde] applying compression after serialization,
+ * and applying decompression before deserialization.
+ *
+ * Note: Compression is not super effective on objects with low repeatabilty of strings.
+ */
 class CompressingSerde<T : Any>(private val codec: Codec, private val serde: Serde<T>) : Serde<T> {
 
    override fun serialize(obj: T): ByteArray {
