626a355ebba633354c8f491959caf45f4f1c3a6b
samsam <sam@sksamuel.com>
2025-05-15 15:36:29 -0500

    Bumped orc

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,30 +1,30 @@
 object Libs {
 
-  const val kotlinVersion = "1.8.21"
-  const val org = "com.sksamuel.centurion"
+   const val kotlinVersion = "1.8.21"
+   const val org = "com.sksamuel.centurion"
 
-  object Kotlin {
-    const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion"
-    const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
-    const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:0.2.0"
-    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC"
-  }
+   object Kotlin {
+      const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion"
+      const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+      const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:0.2.0"
+      const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC"
+   }
 
-  object Parquet {
-    const val common = "org.apache.parquet:parquet-common:1.11.2"
-    const val hadoop = "org.apache.parquet:parquet-hadoop:1.11.2"
-  }
+   object Parquet {
+      const val common = "org.apache.parquet:parquet-common:1.11.2"
+      const val hadoop = "org.apache.parquet:parquet-hadoop:1.11.2"
+   }
 
-  object Orc {
-    const val core = "org.apache.orc:orc-core:1.7.1"
-  }
+   object Orc {
+      const val core = "org.apache.orc:orc-core:2.1.2"
+   }
 
-  object Hadoop {
-    const val common = "org.apache.hadoop:hadoop-common:2.10.1"
-    const val client = "org.apache.hadoop:hadoop-client:2.10.1"
-  }
+   object Hadoop {
+      const val common = "org.apache.hadoop:hadoop-common:2.10.1"
+      const val client = "org.apache.hadoop:hadoop-client:2.10.1"
+   }
 }
 
 object Projects {
-  const val schemas = ":centurion-schemas"
+   const val schemas = ":centurion-schemas"
 }
