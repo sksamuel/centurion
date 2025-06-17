e48546ca5dedc16f580796f0f98a79045f5f6be3
sksamuel <sam@sksamuel.com>
2025-06-17 14:53:28 -0500

    Added GenericRecordEncoder

--- centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/encoders/GenericRecordEncoder.kt
+++ centurion-avro/src/main/kotlin/com/sksamuel/centurion/avro/encoders/GenericRecordEncoder.kt
@@ -3,6 +3,6 @@ package com.sksamuel.centurion.avro.encoders
 import org.apache.avro.Schema
 import org.apache.avro.generic.GenericRecord
 
-abstract class GenericRecordEncoder<T> : Encoder<T> {
-   abstract override fun encode(schema: Schema, value: T): GenericRecord
+class GenericRecordEncoder<T>(val encoder: Encoder<T>) : Encoder<T> {
+   override fun encode(schema: Schema, value: T): GenericRecord = encoder.encode(schema, value) as GenericRecord
 }
