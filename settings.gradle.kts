e2a492d9876c21b6b06776dcbf73b54c69fddd70
samsam <sam@sksamuel.com>
2025-08-05 12:11:13 -0500

    Added foojay

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,10 @@ pluginManagement {
    }
 }
 
+plugins {
+   id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
