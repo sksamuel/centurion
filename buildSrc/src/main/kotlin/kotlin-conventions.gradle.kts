5cd510f0d4748d993f79091c44e8c70afd441d89
sksamuel <sam@sksamuel.com>
2025-08-18 22:32:57 -0500

    Update Kotlin and Kotest dependency versions in build configuration
    Updated `kotlin-stdlib` to version `2.2.10` and replaced local Kotest plugin version `6.0.0-LOCAL` with stable `6.0.0`.

--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -38,9 +38,9 @@ kotlin {
 }
 
 dependencies {
-   testImplementation("org.jetbrains.kotlin:kotlin-stdlib:2.2.0")
+   testImplementation("org.jetbrains.kotlin:kotlin-stdlib:2.2.10")
 
-   val kotestVersion = "6.0.0-LOCAL"
+   val kotestVersion = "6.0.0"
    testImplementation("io.kotest:kotest-framework-engine:$kotestVersion")
    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
