caeba91714889df8cccd7eb72503281b7d1a9d30
sksamuel <sam@sksamuel.com>
2025-09-02 18:00:59 -0500

    Add support for data classes with primitive lists in Gradle plugin tests

--- centurion-avro-gradle-plugin/src/test/kotlin/com/sksamuel/centurion/avro/gradle/DataClassBuilderTest.kt
+++ centurion-avro-gradle-plugin/src/test/kotlin/com/sksamuel/centurion/avro/gradle/DataClassBuilderTest.kt
@@ -12,7 +12,16 @@ class DataClassBuilderTest : FunSpec({
          .name("a").type().intType().noDefault()
          .name("b").type().stringType().noDefault()
          .endRecord()
-      DataClassBuilder().build(schema).toString().shouldMatchResource("/record_with_primitives.kt", trim = true)
+      DataClassBuilder().build(schema).toString().shouldMatchResource("/record_with_primitives.txt", trim = true)
    }
 
+   test("record with primitive lists") {
+      val schema = SchemaBuilder.builder().record("Foo").namespace("com.sksamuel.centurion")
+         .fields()
+         .name("a").type().intType().noDefault()
+         .name("b").type().stringType().noDefault()
+         .name("c").type().array().items(SchemaBuilder.builder().stringType()).noDefault()
+         .endRecord()
+      DataClassBuilder().build(schema).toString().shouldMatchResource("/record_with_primitive_lists.txt", trim = true)
+   }
 })
